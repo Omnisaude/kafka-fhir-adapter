9f0d0151e9b75796892da6a6c26690bf106c5481
Daniel <daniel.nogueira.dacosta@gmail.com>
2024-12-03 15:29:41 -0300

    Parse Patient, baseado e casos de teste

--- src/resources/patient.py
+++ src/resources/patient.py
@@ -15,169 +15,176 @@ from fhir.resources.R4B.period import Period
 # from src.fhir_service import epoch_timestamp_to_iso_string, string_to_bool
 
 
-def init_patient(message: dict) -> str:
-    patient = Patient(
-        name=[
-            HumanName(
-                text=message.get('NOME_PACIENTE', None),
-            )
-        ],
-        identifier=[],
-        extension=[],
-        telecom=[],
-        address=[]
-    )
-
-    meta = Meta(
+def init_patient(message: dict) -> Patient:
+    # Crie o paciente vazio e defina meta
+    patient = Patient()
+    patient.meta = Meta(
         profile=[
             "https://fhir.omnisaude.co/r4/core/StructureDefinition/paciente"
         ]
     )
-    patient.meta = meta
 
-    if message.get('DATA_NASCIMENTO', None):
-        patient_birthDate = message.get('DATA_NASCIMENTO', None)
-        patient.birthDate = patient_birthDate
+    # Adiciona o nome se disponível
+    if message.get('NOME_PACIENTE'):
+        patient.name = [HumanName(text=message['NOME_PACIENTE'])]
 
-    if message.get('CODIGO_SEXO', None):
-        patient_sexo = message.get('CODIGO_SEXO', None)
-        patient.gender = patient_sexo.lower()
+    # Data de nascimento
+    if message.get('DATA_NASCIMENTO'):
+        patient.birthDate = message['DATA_NASCIMENTO']
 
-    if message.get('PRONTUARIO', None):
-        identifier_prontuario = Identifier(
+    # Gênero
+    if message.get('CODIGO_SEXO'):
+        patient.gender = message['CODIGO_SEXO'].lower()
+
+    # Identificadores
+    identifiers = []
+    if message.get('PRONTUARIO'):
+        identifiers.append(Identifier(
             system="https://fhir.omnisaude.co/r4/core/sid/numero-prontuario-americas-health",
-            value=message.get('PRONTUARIO', None)
-        )
-        patient.identifier.append(identifier_prontuario)
+            value=message['PRONTUARIO']
+        ))
 
-    if message.get('CPF', None):
-        identifier_cpf = Identifier(
+    if message.get('CPF'):
+        identifiers.append(Identifier(
             system="https://fhir.omnisaude.co/r4/core/sid/cpf",
-            value=message.get('CPF', None)
-        )
-        patient.identifier.append(identifier_cpf)
+            value=message['CPF']
+        ))
 
-    if message.get('CARTAO_NACIONAL_SAUDE', None):
-        identifier_cns = Identifier(
+    if message.get('CARTAO_NACIONAL_SAUDE'):
+        identifiers.append(Identifier(
             system="https://fhir.omnisaude.co/r4/core/sid/cns",
-            value=message.get('CARTAO_NACIONAL_SAUDE', None)
-        )
-        patient.identifier.append(identifier_cns)
+            value=message['CARTAO_NACIONAL_SAUDE']
+        ))
 
-    if message.get("NOME_MAE", None):
-        extension_mae = Extension(
+    if identifiers:
+        patient.identifier = identifiers
+
+    # Extensões
+    extensions = []
+    if message.get("NOME_MAE"):
+        extensions.append(Extension(
             url="http://hl7.org/fhir/StructureDefinition/patient-mothersMaidenName",
-            valueString=message.get('NOME_MAE', None)
-        )
-        patient.extension.append(extension_mae)
+            valueString=message['NOME_MAE']
+        ))
 
-    if message.get("NOME_PAI", None):
-        extension_pai = Extension(
+    if message.get("NOME_PAI"):
+        extensions.append(Extension(
             url="https://fhir.omnisaude.co/r4/core/StructureDefinition/nome-pai-paciente",
-            valueString=message.get('NOME_PAI', None)
-        )
-        patient.extension.append(extension_pai)
+            valueString=message['NOME_PAI']
+        ))
 
-    if message.get("CODIGO_NACIONALIDADE", None):
+    if message.get("CODIGO_NACIONALIDADE"):
         coding_nacionalidade = Coding(
             system="http://www.saude.gov.br/fhir/r4/CodeSystem/BRPais",
-            # Sistema de codificação (SNOMED CT por exemplo)
-            code=message.get("CODIGO_NACIONALIDADE", None),  # Código que representa um conceito
+            code=message["CODIGO_NACIONALIDADE"]
         )
-
-        codeable_concept_nacionalidade = CodeableConcept(
-            coding=[coding_nacionalidade],  # Lista de codificações, pois pode ter múltiplas
-        )
-
-        extension_nacionalidade = Extension(
+        codeable_concept_nacionalidade = CodeableConcept(coding=[coding_nacionalidade])
+        extensions.append(Extension(
             url="http://hl7.org/fhir/StructureDefinition/patient-nationality",
             extension=[
                 Extension(url="code", valueCodeableConcept=codeable_concept_nacionalidade)
-                # Corrigir para usar valueCode
             ]
-        )
+        ))
 
-        patient.extension.append(extension_nacionalidade)
-
-    if message.get("CODIGO_RACA", None):
-        coding_raca = Coding(
-            system="http://www.saude.gov.br/fhir/r4/CodeSystem/BRRacaCor",
-            # Sistema de codificação (SNOMED CT por exemplo)
-            code=message.get("CODIGO_RACA", None),  # Código que representa um conceito
-        )
-
-        codeable_concept_raca = CodeableConcept(
-            coding=[coding_raca],  # Lista de codificações, pois pode ter múltiplas
-        )
-
-        extension_raca = Extension(
+    if message.get("CODIGO_RACA") or message.get("CODIGO_ETNIA"):
+        extension_raca_cor_etnia = Extension(
             url="http://www.saude.gov.br/fhir/r4/StructureDefinition/BRRacaCorEtnia-1.0",
-            extension=[
-                Extension(url="race", valueCodeableConcept=codeable_concept_raca)
-            ]
+            extension=[]
         )
-        patient.extension.append(extension_raca)
+        
+        if message.get("CODIGO_RACA"):
+            coding_raca = Coding(
+                system="http://www.saude.gov.br/fhir/r4/CodeSystem/BRRacaCor",
+                code=message["CODIGO_RACA"]
+            )
+            
+            codeable_concept_raca = CodeableConcept(coding=[coding_raca])
+            
+            extension_raca_cor_etnia.extension.append(Extension(url="race", valueCodeableConcept=codeable_concept_raca))
 
-    if message.get("CODIGO_ETNIA", None):
-        coding_etnia = Coding(
-            system="http://www.saude.gov.br/fhir/r4/CodeSystem/BREtniaIndigena",
-            # Sistema de codificação (SNOMED CT por exemplo)
-            code=message.get("CODIGO_ETNIA", None),  # Código que representa um conceito
-        )
 
-        codeable_concept_etnia = CodeableConcept(
-            coding=[coding_etnia],  # Lista de codificações, pois pode ter múltiplas
-        )
+        if message.get("CODIGO_ETNIA"):
+            coding_etnia = Coding(
+                system="http://www.saude.gov.br/fhir/r4/CodeSystem/BREtniaIndigena",
+                code=message["CODIGO_ETNIA"]
+            )
+            
+            codeable_concept_etnia = CodeableConcept(coding=[coding_etnia])
+            
+            extension_raca_cor_etnia.extension.append(Extension(url="indigenousEthnicity", valueCodeableConcept=codeable_concept_etnia))
+            
+            
+        extensions.append(extension_raca_cor_etnia)
+            
+    if extensions:
+        patient.extension = extensions
 
-        extension_etnia = Extension(
-            url="http://www.saude.gov.br/fhir/r4/StructureDefinition/BRRacaCorEtnia-1.0",
-            extension=[
-                Extension(url="indigenousEthnicity", valueCodeableConcept=codeable_concept_etnia)
-            ]
-        )
-        patient.extension.append(extension_etnia)
-
+    # Telefone
     if message.get("TELEFONE"):
-        telecom = ContactPoint(
-            system="phone",
-            value=message.get('TELEFONE', None),
-        )
-        patient.telecom.append(telecom)
+        patient.telecom = [ContactPoint(system="phone", value=message['TELEFONE'])]
+        
+    endereco = create_address(message)
+    
+    if not is_address_empty(endereco):
+        patient.address=[]
+        patient.address.append(endereco)
 
-    line_values = [
-        ("BAIRRO", "bairro"),
-        ("COMPLEMENTO", "complemento"),
-        ("NUMERO", "numero"),
-        ("LOGRADOURO", "logradouro"),
-        ("TIPO_LOGRADOURO", "tipoLogradouro"),
-    ]
+    return patient
+    
 
-    line = []
-    _line = []
+def create_address(message: dict) -> Address:
+    address = Address()
+    
+    address_mapping = {
+        "country": "CODIGO_PAIS",
+        "state": "CODIGO_IBGE_ESTADO",
+        "city": "CODIGO_IBGE_CIDADE",
+        "postalCode": "CEP",
+        "text": "ENDERECO_COMPLETO"
+    }
+    
+    for fhir_element, message_field in address_mapping.items():
+        value = message.get(message_field)
+        if value is not None:
+            setattr(address, fhir_element, value)
+            
+    # line_id, message_field
+    line_mapping = {
+        "tipoLogradouro": "CODIGO_TIPO_LOGRADOURO",
+        "logradouro": "LOGRADOURO",
+        "numero": "NUMERO",
+        "complemento": "COMPLEMENTO",
+        "bairro": "BAIRRO"
+    }
+    
+    if any(message.get(message_field) is not None for message_field in line_mapping.values()):
+        address.line=[]
+        address.line__ext=[]
+    
+        for line_id, message_field in line_mapping.items():
+            value = message.get(message_field)
+            if value is not None:
+                address.line.append(value)
+                address.line__ext.append(FHIRPrimitiveExtension(id=line_id, extension=[]))
+    
+    return address
+        
+def is_address_empty(address: Address) -> bool:
+    if any([
+            address.use,
+            address.type,
+            address.text,
+            address.line,
+            address.city,
+            address.district,
+            address.state,
+            address.postalCode,
+            address.country,
+            address.period
+            ]):
+        return False
 
-    # Iterar pelos valores e adicionar apenas os existentes
-    for field, field_id in line_values:
-        value = message.get(field)  # Obter o valor do campo no dicionário
-        if value:  # Adicionar à lista somente se o valor for válido (não vazio)
-            line.append(value)
-            _line.append(FHIRPrimitiveExtension(id=field_id, extension=[]))
+    return True
 
-    # Criar o Address com verificação para `line` e `_line`
-    address = Address(
-        country=message.get("CODIGO_PAIS"),
-        state=message.get("CODIGO_IBGE_ESTADO"),
-        city=message.get("CODIGO_IBGE_CIDADE"),
-        postalCode=message.get("CEP"),
-        text=message.get("ENDERECO_COMPLETO"),
-        line=line if line else None,  # Somente incluir `line` se tiver valores
-        _line=_line if _line else None,  # Somente incluir `_line` se tiver valores
-    )
 
-    # Garantir que a lista de endereços do paciente exista
-    if not hasattr(patient, "address") or patient.address is None:
-        patient.address = []
-
-    # Adicionar o endereço à lista do paciente
-    patient.address.append(address)
-
-    return patient.json()
+export = init_patient, create_address, is_address_empty
