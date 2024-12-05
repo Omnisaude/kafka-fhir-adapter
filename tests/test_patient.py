fc9a3abea3dc23d5de228c333a9f5687e50f67ca
Daniel <daniel.nogueira.dacosta@gmail.com>
2024-12-05 09:02:41 -0300

    Refactor testes para usar lib pytest

--- tests/test_patient.py
+++ tests/test_patient.py
@@ -1,7 +1,4 @@
-import unittest
 import json
-from fhir.resources.R4B.address import Address
-from fhir.resources.R4B.fhirprimitiveextension import FHIRPrimitiveExtension
 
 from kafka_fhir_adapter.resources.patient import is_address_empty, PatientResource
 
@@ -61,94 +58,91 @@ no_address_message_patient = '''{
 }'''
 
 
-class TestPaciente(unittest.TestCase):    
   
-  def test_create_patient_complete(self):
-    message = json.loads(complete_message_patient)
-    
-    patient_resource = PatientResource.from_dict(message)
-    patient = patient_resource.to_fhir()
-    
-    self.assertEqual(patient.name[0].text, "Testevaldo da Silva")
-    self.assertEqual(patient.birthDate.strftime("%Y-%m-%d"), "1957-08-12")
-    self.assertEqual(patient.gender, "female")
-    self.assertEqual(patient.identifier[0].system, "https://fhir.omnisaude.co/r4/core/sid/numero-prontuario-americas-health")
-    self.assertEqual(patient.identifier[0].value, "123234")
-    self.assertEqual(patient.identifier[1].system, "https://fhir.omnisaude.co/r4/core/sid/cpf")
-    self.assertEqual(patient.identifier[1].value, "123234")
-    self.assertEqual(patient.extension[0].url, "http://hl7.org/fhir/StructureDefinition/patient-mothersMaidenName")
-    self.assertEqual(patient.extension[0].valueString, "Mãeteste da Silva")
-    self.assertEqual(patient.extension[1].url, "https://fhir.omnisaude.co/r4/core/StructureDefinition/nome-pai-paciente")
-    self.assertEqual(patient.extension[1].valueString, "Paiteste da Silva")
-    self.assertEqual(patient.extension[2].url, "http://hl7.org/fhir/StructureDefinition/patient-nationality")
-    self.assertEqual(patient.extension[2].extension[0].valueCodeableConcept.coding[0].system, "http://www.saude.gov.br/fhir/r4/CodeSystem/BRPais")
-    self.assertEqual(patient.extension[2].extension[0].valueCodeableConcept.coding[0].code, "10")
-    self.assertEqual(patient.extension[3].url, "http://www.saude.gov.br/fhir/r4/StructureDefinition/BRRacaCorEtnia-1.0")
-    self.assertEqual(patient.extension[3].extension[0].valueCodeableConcept.coding[0].system, "http://www.saude.gov.br/fhir/r4/CodeSystem/BRRacaCor")
-    self.assertEqual(patient.extension[3].extension[0].valueCodeableConcept.coding[0].code, "01")
-    self.assertEqual(patient.address[0].country, "BRA")
-    self.assertEqual(patient.address[0].state, "17")
-    self.assertEqual(patient.address[0].city, "171820")
-    self.assertEqual(patient.address[0].postalCode, "77500000")
-    self.assertEqual(patient.address[0].text, "Rua da Silva, 123 , Centro , 77500000, Porto Nacional - TO, Brasil")
-    self.assertEqual(patient.address[0].line[0], "Rua da Silva ")
-    self.assertEqual(patient.address[0].line[1], "123")
-    self.assertEqual(patient.address[0].line[2], "Centro ")
-    self.assertEqual(patient.address[0].line__ext[0].id, "logradouro")
-    self.assertEqual(patient.address[0].line__ext[1].id, "numero")
-    self.assertEqual(patient.address[0].line__ext[2].id, "bairro")
   
-  def test_is_address_empty_true(self):
-      message = json.loads(no_address_message_patient)
-      
-      patient_resource = PatientResource.from_dict(message)
-      
-      endereco = patient_resource.create_address()
-      
-      self.assertTrue(is_address_empty(endereco))
+def test_create_patient_complete():
+  message = json.loads(complete_message_patient)
 
-  def test_is_address_empty_false(self):
-      message = json.loads(complete_message_patient)
-      
-      patient_resource = PatientResource.from_dict(message)
+  patient_resource = PatientResource.from_dict(message)
+  patient = patient_resource.to_fhir()
 
-      endereco = patient_resource.create_address()
-      
-      self.assertFalse(is_address_empty(endereco))
+  assert patient.name[0].text == "Testevaldo da Silva"
+  assert patient.birthDate.strftime("%Y-%m-%d") == "1957-08-12"
+  assert patient.gender == "female"
+  assert patient.identifier[0].system == "https://fhir.omnisaude.co/r4/core/sid/numero-prontuario-americas-health"
+  assert patient.identifier[0].value == "123234"
+  assert patient.identifier[1].system == "https://fhir.omnisaude.co/r4/core/sid/cpf"
+  assert patient.identifier[1].value == "123234"
+  assert patient.extension[0].url == "http://hl7.org/fhir/StructureDefinition/patient-mothersMaidenName"
+  assert patient.extension[0].valueString == "Mãeteste da Silva"
+  assert patient.extension[1].url == "https://fhir.omnisaude.co/r4/core/StructureDefinition/nome-pai-paciente"
+  assert patient.extension[1].valueString == "Paiteste da Silva"
+  assert patient.extension[2].url == "http://hl7.org/fhir/StructureDefinition/patient-nationality"
+  assert patient.extension[2].extension[0].valueCodeableConcept.coding[0].system == "http://www.saude.gov.br/fhir/r4/CodeSystem/BRPais"
+  assert patient.extension[2].extension[0].valueCodeableConcept.coding[0].code == "10"
+  assert patient.extension[3].url == "http://www.saude.gov.br/fhir/r4/StructureDefinition/BRRacaCorEtnia-1.0"
+  assert patient.extension[3].extension[0].valueCodeableConcept.coding[0].system == "http://www.saude.gov.br/fhir/r4/CodeSystem/BRRacaCor"
+  assert patient.extension[3].extension[0].valueCodeableConcept.coding[0].code == "01"
+  assert patient.address[0].country == "BRA"
+  assert patient.address[0].state == "17"
+  assert patient.address[0].city == "171820"
+  assert patient.address[0].postalCode == "77500000"
+  assert patient.address[0].text == "Rua da Silva, 123 , Centro , 77500000, Porto Nacional - TO, Brasil"
+  assert patient.address[0].line[0] == "Rua da Silva "
+  assert patient.address[0].line[1] == "123"
+  assert patient.address[0].line[2] == "Centro "
+  assert patient.address[0].line__ext[0].id == "logradouro"
+  assert patient.address[0].line__ext[1].id == "numero"
+  assert patient.address[0].line__ext[2].id == "bairro"
 
-  def test_create_address_complete(self):
-      message = json.loads(complete_message_patient)
-      
-      patient_resource = PatientResource.from_dict(message)
-      
-      endereco = patient_resource.create_address()
-      
-      self.assertEqual(endereco.country, "BRA")
-      self.assertEqual(endereco.state, "17")
-      self.assertEqual(endereco.city, "171820")
-      self.assertEqual(endereco.postalCode, "77500000")
-      self.assertEqual(endereco.text, "Rua da Silva, 123 , Centro , 77500000, Porto Nacional - TO, Brasil")
-      self.assertEqual(endereco.line[0], "Rua da Silva ")
-      self.assertEqual(endereco.line[1], "123")
-      self.assertEqual(endereco.line[2], "Centro ")        
-      self.assertEqual(endereco.line__ext[0].id, "logradouro")
-      self.assertEqual(endereco.line__ext[1].id, "numero")
-      self.assertEqual(endereco.line__ext[2].id, "bairro")
+def test_is_address_empty_true():
+  message = json.loads(no_address_message_patient)
+  
+  patient_resource = PatientResource.from_dict(message)
+  
+  endereco = patient_resource.create_address()
+  
+  assert is_address_empty(endereco)
 
-  def test_create_address_empty(self):
-      message = json.loads(no_address_message_patient)
+def test_is_address_empty_false():
+  message = json.loads(complete_message_patient)
+  
+  patient_resource = PatientResource.from_dict(message)
 
-      patient_resource = PatientResource.from_dict(message)
-      
-      endereco = patient_resource.create_address()
-      
-      self.assertIsNone(endereco.country)
-      self.assertIsNone(endereco.state)
-      self.assertIsNone(endereco.city)
-      self.assertIsNone(endereco.postalCode)
-      self.assertIsNone(endereco.text)
-      self.assertIsNone(endereco.line)
-      self.assertIsNone(endereco.line__ext)
+  endereco = patient_resource.create_address()
+  
+  assert not is_address_empty(endereco)
 
-if __name__ == '__main__':
-    unittest.main()
+def test_create_address_complete():
+  message = json.loads(complete_message_patient)
+  
+  patient_resource = PatientResource.from_dict(message)
+  
+  endereco = patient_resource.create_address()
+  
+  assert endereco.country == "BRA"
+  assert endereco.state == "17"
+  assert endereco.city == "171820"
+  assert endereco.postalCode == "77500000"
+  assert endereco.text == "Rua da Silva, 123 , Centro , 77500000, Porto Nacional - TO, Brasil"
+  assert endereco.line[0] == "Rua da Silva "
+  assert endereco.line[1] == "123"
+  assert endereco.line[2] == "Centro "        
+  assert endereco.line__ext[0].id == "logradouro"
+  assert endereco.line__ext[1].id == "numero"
+  assert endereco.line__ext[2].id == "bairro"
+
+def test_create_address_empty():
+  message = json.loads(no_address_message_patient)
+
+  patient_resource = PatientResource.from_dict(message)
+  
+  endereco = patient_resource.create_address()
+  
+  assert endereco.country is None
+  assert endereco.state is None
+  assert endereco.city is None
+  assert endereco.postalCode is None
+  assert endereco.text is None
+  assert endereco.line is None
+  assert endereco.line__ext is None
