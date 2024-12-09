fc585ddb254d5fdeae10d409e4c93afc255fd471
Guilherme Reis <guilhermejuizo2@gmail.com>
2024-12-09 16:33:36 -0300

    Subindo Practitioner resource e consumer

---
+++ kafka_fhir_adapter/resources/practitioner.py
@@ -0,0 +1,189 @@
+from dataclasses import dataclass
+from typing import Optional
+
+from fhir.resources.R4B.practitioner import Practitioner
+from fhir.resources.R4B.humanname import HumanName
+from fhir.resources.R4B.identifier import Identifier
+from fhir.resources.R4B.address import Address
+from fhir.resources.R4B.contactpoint import ContactPoint
+from fhir.resources.R4B.meta import Meta
+from fhir.resources.R4B.fhirprimitiveextension import FHIRPrimitiveExtension
+from fhir.resources.R4B.period import Period
+from fhir.resources.R4B.extension import Extension
+
+@dataclass
+class PractitionerResource:
+    id: Optional[str]
+    codigo_pessoa_fisica: Optional[str]
+    nome_profissional: Optional[str]
+    cpf: Optional[str]
+    data_nascimento: Optional[str]
+    codigo_tipo_logradouro: Optional[str]
+    logradouro: Optional[str]
+    numero: Optional[str]
+    complemento: Optional[str]
+    bairro: Optional[str]
+    codigo_ibge_cidade: Optional[str]
+    codigo_ibge_estado: Optional[str]
+    codigo_pais: Optional[str]
+    cep: Optional[str]
+    endereco_completo: Optional[str]
+    nr_conselho: Optional[str]
+    codigo_sexo: Optional[str]
+    telefone: Optional[str]
+    data_atualizacao: Optional[str]
+
+    @classmethod
+    def from_dict(cls, message: dict):
+        return cls(
+            id=None,
+            codigo_pessoa_fisica=message.get('CD_PESSOA_FISICA', None),
+            nome_profissional=message.get('NOME_PROFISSIONAL', None),
+            cpf=message.get('CPF', None),
+            data_nascimento=message.get('DATA_NASCIMENTO', None),
+            codigo_tipo_logradouro=message.get('CODIGO_TIPO_LOGRADOURO', None),
+            logradouro=message.get('LOGRADOURO', None),
+            numero=message.get('NUMERO', None),
+            complemento=message.get('COMPLEMENTO', None),
+            bairro=message.get('BAIRRO', None),
+            codigo_ibge_cidade=message.get('CODIGO_IBGE_CIDADE', None),
+            codigo_ibge_estado=message.get('CODIGO_IBGE_ESTADO', None),
+            codigo_pais=message.get('CODIGO_PAIS', None),
+            cep=message.get('CEP', None),
+            endereco_completo=message.get('ENDERECO_COMPLETO', None),
+            nr_conselho=message.get('NR_CONSELHO', None),
+            codigo_sexo=message.get('CODIGO_SEXO', None),
+            telefone=message.get('TELEFONE', None),
+            data_atualizacao=message.get('DATA_ATUALIZACAO', None)
+        )
+
+    def to_fhir(self) -> Practitioner:
+        practitioner = Practitioner(
+            name=[],
+            identifier=[],
+            address=[],
+            telecom=[],
+            extension=[]
+        )
+
+        meta = Meta(
+            profile=[
+                "https://fhir.omnisaude.co/r4/core/StructureDefinition/profissional"
+            ]
+        )
+        practitioner.meta = meta
+
+        if self.nome_profissional:
+            name = HumanName(
+                text=self.nome_profissional
+            )
+            practitioner.name.append(name)
+
+        if self.cpf:
+            identifier_cpf = Identifier(
+                system="https://fhir.omnisaude.co/r4/core/sid/cpf",
+                value=self.cpf
+            )
+            practitioner.identifier.append(identifier_cpf)
+
+        if self.nr_conselho:
+            identifier_crm = Identifier(
+                system="https://fhir.omnisaude.co/r4/core/sid/crm",
+                value=self.nr_conselho
+            )
+            practitioner.identifier.append(identifier_crm)
+
+        if self.codigo_pessoa_fisica:
+            identifier_internal = Identifier(
+                system="https://fhir.omnisaude.co/r4/core/sid/cd_pessoa_fisica",
+                value=self.codigo_pessoa_fisica
+            )
+            practitioner.identifier.append(identifier_internal)
+
+        if self.codigo_sexo:
+            practitioner.gender = self.codigo_sexo.lower()
+
+        if self.data_nascimento:
+            practitioner.birthDate = self.data_nascimento
+
+        endereco = self.create_address()
+        
+        if not is_address_empty(endereco):
+            practitioner.address=[]
+            practitioner.address.append(endereco)
+
+        if self.telefone:
+            telecom = ContactPoint(
+                system="phone",
+                value=self.telefone,
+                use="mobile"
+            )
+            practitioner.telecom.append(telecom)
+
+        return practitioner
+    
+    def create_address(self) -> Address:
+        address = Address()
+        
+        if self.codigo_pais:
+            address.country = self.codigo_pais
+        
+        if self.codigo_ibge_estado:
+            address.state = self.codigo_ibge_estado
+        
+        if self.codigo_ibge_cidade:
+            address.city = self.codigo_ibge_cidade
+        
+        if self.cep:
+            address.postalCode = self.cep
+        
+        if self.endereco_completo:
+            address.text = self.endereco_completo
+        
+        line=[]
+        _line=[]
+        
+        if self.codigo_tipo_logradouro:
+            line.append(self.codigo_tipo_logradouro)
+            _line.append(FHIRPrimitiveExtension(id="tipoLogradouro", extension=[]))
+        
+        if self.logradouro:
+            line.append(self.logradouro)
+            _line.append(FHIRPrimitiveExtension(id="logradouro", extension=[]))
+        
+        if self.numero:
+            line.append(self.numero)
+            _line.append(FHIRPrimitiveExtension(id="numero", extension=[]))
+            
+        if self.complemento:
+            line.append(self.complemento)
+            _line.append(FHIRPrimitiveExtension(id="complemento", extension=[]))
+            
+        if self.bairro:
+            line.append(self.bairro)
+            _line.append(FHIRPrimitiveExtension(id="bairro", extension=[]))
+        
+        if line:    
+            address.line=line
+            address.line__ext=_line
+        
+        return address
+            
+def is_address_empty(address: Address) -> bool:
+    if any([
+            address.use,
+            address.type,
+            address.text,
+            address.line,
+            address.city,
+            address.district,
+            address.state,
+            address.postalCode,
+            address.country,
+            address.period
+            ]):
+        return False
+
+    return True
+
+export = is_address_empty, PractitionerResource
---
+++ tests/kafka_fhir_adapter/resources/test_practitioner.py
@@ -0,0 +1,45 @@
+import json
+from kafka_fhir_adapter.resources.practitioner import PractitionerResource
+import pytest
+
+complete_message_practitioner = '''{
+    "CD_PESSOA_FISICA": "7891011",
+    "NOME_PROFISSIONAL": "João da Silva",
+    "CPF": "123.456.789-00",
+    "DATA_NASCIMENTO": "1985-07-23",
+    "CODIGO_TIPO_LOGRADOURO": null,
+    "LOGRADOURO": "Rua das Flores",
+    "NUMERO": "123",
+    "COMPLEMENTO": null,
+    "BAIRRO": "Jardim Primavera",
+    "CODIGO_IBGE_CIDADE": "1234567",
+    "CODIGO_IBGE_ESTADO": "12",
+    "CODIGO_PAIS": "BRA",
+    "CEP": "12345678",
+    "ENDERECO_COMPLETO": "Rua das Flores, 123, Apartamento 45, Jardim Primavera, 12345678, Brasil",
+    "NR_CONSELHO": "CRM123456",
+    "CODIGO_SEXO": "1",
+    "TELEFONE": "+55 (11) 98765-4321"
+  }'''
+
+
+def test_create_condition_complete():
+  message = json.loads(complete_message_practitioner)
+
+  practitioner_resource = PractitionerResource.from_dict(message)
+  practitioner = practitioner_resource.to_fhir()
+  print("Practitioner Resource:", practitioner_resource)
+  print("Practitioner Object:", practitioner)
+  assert practitioner.name[0].text == "João da Silva"
+  assert practitioner.meta.profile[0] == "https://fhir.omnisaude.co/r4/core/StructureDefinition/profissional"
+  assert practitioner.address[0].country == "BRA"
+  assert practitioner.address[0].state == "12"
+  assert practitioner.address[0].city == "1234567"
+  assert practitioner.address[0].postalCode == "12345678"
+  assert practitioner.address[0].text == "Rua das Flores, 123, Apartamento 45, Jardim Primavera, 12345678, Brasil"
+  assert practitioner.address[0].line[0] == "Rua das Flores"
+  assert practitioner.address[0].line[1] == "123"
+  assert practitioner.address[0].line[2] == "Jardim Primavera"
+  assert practitioner.address[0].line__ext[0].id == "logradouro"
+  assert practitioner.address[0].line__ext[1].id == "numero"
+  assert practitioner.address[0].line__ext[2].id == "bairro"
